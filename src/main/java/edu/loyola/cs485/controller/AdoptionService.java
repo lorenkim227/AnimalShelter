e0da492a2e06b4730002212d094fdb026f299268
chloemiranda <cmmiranda@loyola.edu>
2025-12-02 14:33:53 -0500

    added outlines for dao and controllers for adoption, following mvc architecture

---
+++ src/main/java/edu/loyola/cs485/controller/AdoptionService.java
@@ -0,0 +1,33 @@
+package edu.loyola.cs485.controller;
+
+import edu.loyola.cs485.model.dao.AdoptionDAO;
+import edu.loyola.cs485.model.entity.Adoption;
+import java.util.List;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * For an MVC architecture, we need another class to separate the Interface (View) from the DAOs (Model)
+ * Even if it seems redudant to you, it is a good practice (make a habit of it).
+ */
+public class AdoptionService {
+
+    public Adoption createAdoption() throws Exception {
+       return null;
+    }
+
+    public Adoption updateAdoption() throws Exception {
+        return null;
+    }
+
+    public Adoption deleteAdoption() throws Exception {
+        return null;
+    }
+
+    public List<Adoption> getAllAdoptions() throws Exception {
+        return null;
+    }
+
+    public void deleteAdoption(int id) throws Exception {
+    }
+}
