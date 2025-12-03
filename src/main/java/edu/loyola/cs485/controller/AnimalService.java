a5e6efe72742f81b722adb17f20c545668795ab1
chloemiranda <cmmiranda@loyola.edu>
2025-12-03 00:06:30 -0500

    added outlines for dao and controllers for adoption, following mvc architecture

---
+++ src/main/java/edu/loyola/cs485/controller/AnimalService.java
@@ -0,0 +1,30 @@
+package edu.loyola.cs485.controller;
+
+import edu.loyola.cs485.model.entity.Animal;
+import java.util.List;
+
+/**
+ * For an MVC architecture, we need another class to separate the Interface (View) from the DAOs (Model)
+ * Even if it seems redudant to you, it is a good practice (make a habit of it).
+ */
+public class AnimalService {
+
+    public Animal createAnimal() throws Exception {
+       return null;
+    }
+
+    public Animal updateAnimal() throws Exception {
+        return null;
+    }
+
+    public Animal deleteAnimal() throws Exception {
+        return null;
+    }
+
+    public List<Animal> getAllAnimals() throws Exception {
+        return null;
+    }
+
+    public void deleteAnimal(int id) throws Exception {
+    }
+}
