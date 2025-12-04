c39d282f933baeee7a2096a06abcbd0d9750febc
lorenkim <lkim1@loyola.edu>
2025-12-04 17:06:12 -0500

    added Jform

---
+++ src/main/java/edu/loyola/cs485/model/dao/AnimalCrudDialog.java
@@ -0,0 +1,12 @@
+package edu.loyola.cs485.model.dao;
+
+import javax.swing.*;
+
+public class AnimalCrudDialog {
+    private JTextField txtName;
+    private JTextField txtBreed;
+    private JTextField txtSpecies;
+    private JTextField txtColor;
+    private JTextField txtDob;
+    private JTextField txtId;
+}
